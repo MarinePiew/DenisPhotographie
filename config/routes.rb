766a787fb73e6a7372d8c7cb6fdec3459d13f3d1
macbook <petitm31@gmail.com>
2017-04-03 12:10:48 +0200

    Add portfoio page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'A_propos', to: 'pages#about'
+  get 'Portfolio', to: 'pages#portfolio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

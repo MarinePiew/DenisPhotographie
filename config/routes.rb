7bd542a755025a638ff6f9315647f31ef2b5403a
macbook <petitm31@gmail.com>
2017-04-04 11:36:04 +0200

    Amélioration des pages et footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'A_propos', to: 'pages#about'
   get 'Portfolio', to: 'pages#portfolio'
+  get 'Contact', to: 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
